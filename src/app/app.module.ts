c94f69e92b0f3dd95502110e3a163ac2ba39d9f8
USER <test@test.com>
2018-04-14 11:52:04 +0900

    2일차 ngTodo - 컴포넌트 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,19 @@ import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MatToolbarModule} from '@angular/material';
 
+const route: Routes = [
+  {path: '', component: IndexComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'angular', component: AngularComponent},
+      {path: 'jquery', component: JqueryComponent}
+    ]}, // 사용자 사이트
+  // {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}, // 관리자 사이트, lazyloading
+
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +30,9 @@ import { AngularComponent } from './angular/angular.component';
     AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route),
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
