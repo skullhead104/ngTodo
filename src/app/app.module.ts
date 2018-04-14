b11e96ec5d01bc930e6778db85bedbfb3708c8fb
USER <test@test.com>
2018-04-14 11:05:42 +0900

    2일차 ngTodo - 컴포넌트 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule
